b0eb53af98e7c5030dcdfc350b4f849586d6386b
AndriiAndronyk <AndriiAndronyk.com>
2020-08-05 03:39:48 +0300

    Event handling and stubs for functions (Analyse, Save top words)

--- main.py
+++ main.py
@@ -3,15 +3,27 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 import gui
 import sys
-class Example(QMainWindow, gui.Ui_Main):
+
+
+class MainWindow(QMainWindow, gui.Ui_Main):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.pushButton.clicked.connect(self.buttonClicked)
-    def buttonClicked(self):
+        self.pushButton.clicked.connect(self.StartAnalyse)
+        self.toolButton.clicked.connect(self.OutputWords)
+
+    def StartAnalyse(self):
         print('There will be magically created with the help of a 🐼 and 🐍 ')
+
+    def OutputWords(self):
+        print('There will words for learning export func ')
+
+    def CloseApp(self):
+        exit()
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    form = Example()
+    form = MainWindow()
     form.show()
     app.exec()
